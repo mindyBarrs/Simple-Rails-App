7ef07acc86885bf3a75773b0e818522a4ecc9d0d
mindy <barrs.mindy@gmail.com>
2018-06-02 15:42:14 -0400

    Added a controller, views and routed them

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ActionController::Base
+    # Creating Methods
+    def index
+    end
+
+    def about
+    end
+
+    def contact
+    end
+
+    def services
+    end
+end
