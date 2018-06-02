fbd780a5cd7660e0ee59dbf22ee5d0e22db9f46e
mindy <barrs.mindy@gmail.com>
2018-06-02 19:53:00 -0400

    Update files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,29 @@
 class PostsController < ApplicationController
   def index
+    # Show all blog posts
   end
 
   def new
+    # Used for new blog posts
   end
 
   def create
+    # Used to create a new blog post
   end
 
   def edit
+    # Edit blog post
   end
 
   def update
+    # Used to update blog posts
   end
 
   def show
+    # Used to show a blog
   end
 
   def destroy
+    # Used to delete a blog
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/show'
-  get 'posts/destroy'
+  resources :posts
   root 'pages#index', as: 'home'
   
   get 'about' => 'pages#about', as: 'about' # <-- That is an Alias
   get 'contact' => 'pages#contact', as: 'contact'
   get 'services' => 'pages#services', as: 'services'
+  
 end
