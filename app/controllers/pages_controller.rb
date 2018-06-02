7cd63a297aa6bb6e69c5190f0772233b46c0d766
mindy <barrs.mindy@gmail.com>
2018-06-02 17:29:42 -0400

    Updated files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,23 @@
 class PagesController < ActionController::Base
     # Creating Methods
     def index
+        @title ="Welcome"
+        @content = "Welcome to the Homepage"
     end
 
     def about
+        @title = "About"
+        @content = "Welcome to the About page"
     end
 
     def contact
+        @title = "Contact"
+        @content = "Welcome to the Contact page"
     end
 
     def services
+        @title = "Service"
+        @content = "Welcome to the Service page"
+        @services = ["Web Design", "Web Development", "SEO"]
     end
 end
